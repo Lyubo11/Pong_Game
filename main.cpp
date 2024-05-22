ecb1a6338a95eed3fc7046fc8a78f673706cde0c
Lyubo11 <lubomirkirilov2004@gmail.com>
2024-05-22 04:20:25 +0300

    Initial setup with main loop and game window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <raylib.h>
+
+const int screen_width = 800;
+const int screen_height = 600;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(screen_width, screen_height, "Pong Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BEIGE);
+
+        DrawText("Pong Game", screen_width / 2 - 55, screen_height / 2 - 15, 20, BROWN);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
